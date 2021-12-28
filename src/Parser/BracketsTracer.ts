9d3227e208574d3bd0b2892358e35dab89b19289
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-12-29 01:36:13 +0200

    Add BracketsTracer.ts

---
+++ src/Parser/BracketsTracer.ts
@@ -0,0 +1,57 @@
+import { LexemeBuffer } from "../Lexer/LexemeBuffer";
+import { LexemeType } from "../Types/LexemeTypes";
+
+export class BracketsTracer {
+    /**
+     * Traces end of subexpression in brackets
+     * @param lexemeList list of lexemes
+     * @param currentLexemeIndex start position to search
+     * @returns index of close bracket lexeme of expression or false if it was not found
+     */
+    public traceBracketsExpression(lexemeList: LexemeBuffer, currentLexemeIndex: number, lexemeType: LexemeType): number | boolean {
+        let bracketsCounter = 1;
+        let currentPosition = currentLexemeIndex;
+
+        if (lexemeType == LexemeType.LEFT_BRACKET)  {
+            return this.traceForward(lexemeList, bracketsCounter, currentPosition);
+        } else if (lexemeType == LexemeType.RIGHT_BRACKET) {
+            return this.traceBackward(lexemeList, bracketsCounter, currentPosition);
+        }
+    }
+
+    /**
+     * Trace expression that starts with opening bracket
+     * @param lexemeList list of lexemes
+     * @param bracketsCounter 
+     * @param currentPosition current position in lexeme list
+     * @returns position of end of the expression or false if it is incorrect
+     */
+    private traceForward(lexemeList: LexemeBuffer, bracketsCounter: number, currentPosition: number): number | boolean {
+        while(bracketsCounter != 0 && currentPosition <= lexemeList.length()) {
+            currentPosition ++;
+
+            lexemeList.get(currentPosition).type == LexemeType.LEFT_BRACKET ? bracketsCounter ++ : null;
+            lexemeList.get(currentPosition).type == LexemeType.RIGHT_BRACKET ? bracketsCounter -- : null;
+        }
+
+        return bracketsCounter == 0 ? currentPosition : false;
+    }
+
+    /**
+     * Trace expression that starts with closing bracket
+     * @param lexemeList list of lexemes
+     * @param bracketsCounter 
+     * @param currentPosition current position in lexeme list
+     * @returns position of end of the expression or false if it is incorrect
+     */
+    private traceBackward(lexemeList: LexemeBuffer, bracketsCounter: number, currentPosition: number): number | boolean {
+        while(bracketsCounter != 0 && currentPosition >= 0) {
+            currentPosition --;
+
+            lexemeList.get(currentPosition).type == LexemeType.RIGHT_BRACKET ? bracketsCounter ++ : null;
+            lexemeList.get(currentPosition).type == LexemeType.LEFT_BRACKET ? bracketsCounter -- : null;
+        }
+
+        return bracketsCounter == 0 ? currentPosition : false;
+    }
+}
