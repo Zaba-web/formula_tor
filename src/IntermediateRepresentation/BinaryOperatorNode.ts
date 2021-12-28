e77c3f5a3a0aaf9838a3e274455dbd96c30140a7
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-12-29 01:34:38 +0200

    Change operand representation from single Node to NodeList

--- src/IntermediateRepresentation/BinaryOperatorNode.ts
+++ src/IntermediateRepresentation/BinaryOperatorNode.ts
@@ -1,13 +1,19 @@
 import { Node } from "./Node";
 import { LexemeType } from "../Types/LexemeTypes";
+import { NodeList } from "./NodeList";
 
 export class BinaryOperatorNode extends Node {
-    private _leftHandSideOperand: Node;
-    private _rightHandSideOperand: Node;
+    private _leftHandSideOperand: NodeList;
+    private _rightHandSideOperand: NodeList;
 
-    constructor(value: string, type: LexemeType) {
+    constructor(value: string, type: LexemeType, lhsOperand?: NodeList, rhsOperand?: NodeList) {
         super(value, type);
         this._arity = 2;
+
+        if(lhsOperand && rhsOperand) {
+            this.leftHandSideOperand = lhsOperand;
+            this.rightHandSideOperand = rhsOperand;
+        }
     }
 
     public get leftHandSideOperand()  {
@@ -18,11 +24,11 @@ export class BinaryOperatorNode extends Node {
         return this._rightHandSideOperand;
     }
 
-    public set leftHandSideOperand(operand: Node) {
+    public set leftHandSideOperand(operand: NodeList) {
         this._leftHandSideOperand = operand;
     }
 
-    public set rightHandSideOperand(operand: Node) {
+    public set rightHandSideOperand(operand: NodeList) {
         this._rightHandSideOperand = operand;
     }
 }
--- src/IntermediateRepresentation/UnaryOperatorNode.ts
+++ src/IntermediateRepresentation/UnaryOperatorNode.ts
@@ -1,15 +1,20 @@
 import { Node } from "./Node";
 import { LexemeType } from "../Types/LexemeTypes";
+import { NodeList } from "./NodeList";
 
 export class UnaryOperatorNode extends Node {
-    private _operand: Node
+    private _operand: NodeList
 
-    constructor(value: string, type: LexemeType) {
+    constructor(value: string, type: LexemeType, operand?: NodeList) {
         super(value, type);
         this._arity = 1;
+
+        if(operand) {
+            this.operand = operand;
+        }
     }
 
-    public set operand(revicedOperand: Node) {
+    public set operand(revicedOperand: NodeList) {
         this._operand = revicedOperand;
     }
 
