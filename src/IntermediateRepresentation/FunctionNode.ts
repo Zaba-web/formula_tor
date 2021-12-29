07248b5447a8777349c5be65cf5b9a910369c024
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-12-29 16:03:22 +0200

    Add node for functions

---
+++ src/IntermediateRepresentation/FunctionNode.ts
@@ -0,0 +1,24 @@
+import { Node } from "./Node";
+import { LexemeType } from "../Types/LexemeTypes";
+import { NodeList } from "./NodeList";
+
+export class FunctionNode extends Node {
+    private _arguments: NodeList[];
+
+    constructor(value: string, type: LexemeType, arity: number, args?:NodeList[]) {
+        super(value, type);
+        this._arity = arity;
+
+        if(args) {
+            this.arguments = args;
+        }
+    }
+
+    set arguments(args: NodeList[]) {
+        this._arguments = args;
+    }
+
+    get arguments() {
+        return this._arguments;
+    }
+}
