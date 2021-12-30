2c4773cc354f6e5ea177a943ac10533420d41723
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-12-30 02:06:34 +0200

    Add new functions

--- src/Types/FunctionTable.ts
+++ src/Types/FunctionTable.ts
@@ -3,12 +3,36 @@ interface FunctionDescription {
 }
 
 interface IFunctionTable {
-    'Root': FunctionDescription
+    'Root': FunctionDescription,
+    'Log': FunctionDescription,
+    'System2': FunctionDescription,
+    'System3': FunctionDescription,
+    'System4': FunctionDescription,
+    'IndefInt': FunctionDescription,
+    'DefInt': FunctionDescription
 }
 
 const FunctionTable = {
     'Root': {
         argCount: 2
+    },
+    'Log': {
+        argCount: 2
+    },
+    'System2': {
+        argCount: 2
+    },
+    'System3': {
+        argCount: 3
+    },
+    'System4': {
+        argCount: 4
+    },
+    'IndefInt': {
+        argCount: 1
+    },
+    'DefInt': {
+        argCount: 3
     }
 }
 
