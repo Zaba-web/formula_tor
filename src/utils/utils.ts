c96bae78def1c761721fb048ed7a569f218be95f
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-12-30 16:06:25 +0200

    Add hasSomeParentTheClass function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,4 +9,9 @@ function getProperty<T, K extends keyof T>(o: T, propertyName: K): T[K] {
     return o[propertyName];
 }
 
-export {getProperty}
+function hasSomeParentTheClass(element: HTMLElement, classname: string): boolean {
+    if (element.className.split(' ').indexOf(classname)>=0) return true;
+    return element.parentNode && hasSomeParentTheClass(element.parentNode as HTMLElement, classname);
+}
+
+export {getProperty, hasSomeParentTheClass}
