fc188a4e9dbd8e8c27239375426bc194037cd5f1
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-12-30 16:06:48 +0200

    Add Formulator class to export for clients

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,43 @@
 import { Lexer } from "./Lexer/Lexer";
 import { Parser } from "./Parser/Parser";
-import { Visualizer } from "./Visualizer/Visualizer";
+import { HTMLVisualizer } from "./Visualizer/HTMLVisualizer/HTMLVisualizer";
+
+class Formulator {
+    private lexer: Lexer;
+    private parser: Parser;
+    private visualizer: HTMLVisualizer;
+
+    constructor (cotnainer: HTMLElement) {
+        this.lexer = new Lexer();
+        this.parser = new Parser();
+        this.visualizer = new HTMLVisualizer(cotnainer);
+    }
+
+    public visualize(expression: string) {
+        const lexemes = this.lexer.analyze(expression);
+        const intermediateRepresentation = this.parser.analyze(lexemes);
+        this.visualizer.visualize(intermediateRepresentation);
+    }
+}
+
+export default Formulator;
+
 
 //const input = document.querySelector('.inp') as HTMLInputElement;
 //const btn = document.querySelector('.submit') as HTMLButtonElement;
 
-const lx = new Lexer();
-const parser = new Parser();
+// const lx = new Lexer();
+// const parser = new Parser();
 
-const str = "x * (10 - 15)";
-const lexemes = lx.analyze(str);
-const ir = parser.analyze(lexemes);
+// const str = "";
+// const lexemes = lx.analyze(str);
+// const ir = parser.analyze(lexemes);
 
-console.log(lexemes.getLexemeList());
-console.log(ir);
+// console.log(lexemes.getLexemeList());
+// console.table(ir);
 
-//const vis = new Visualizer('.test');
-
+// const vis = new HTMLVisualizer(document.querySelector(".test"));
+// vis.visualize(ir);
 //vis.visualize(ir);
 
 // const vis = new Visualizer('.test');
